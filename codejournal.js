2f3b34dad1d4ffb30cb7d8a26af976353bf62eaa
Charley Benedict <charleybug23@gmail.com>
2021-10-07 15:42:58 -0600

    very pretty now

---
+++ codejournal.js
@@ -0,0 +1,14 @@
+// do i even really know what is going on? no. no i do not.
+
+// Boolean
+
+let myBool = false;
+
+// Array [ ]
+
+let myArray = []; // this is an array
+
+//
+let myArray2 = [];
+
+// object { }
