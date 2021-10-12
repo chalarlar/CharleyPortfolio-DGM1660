df1d83aba14cd7354a4272f0ae683fa3085c8f50
Charley Benedict <charleybug23@gmail.com>
2021-10-12 12:45:59 -0600

    fixed what was being weird, added title list

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -6,6 +6,12 @@ let titleList = document.createElement('ol')
 
 filmList.appendChild(titleList)
 
+let poster = document.createElement('img')
+
+poster.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+
+filmList.appendChild(poster)
+
 for (let i = 0; i < films.length; i++) {
     let titleItem = document.createElement('li')
     titleItem.textContent = films[i].title
