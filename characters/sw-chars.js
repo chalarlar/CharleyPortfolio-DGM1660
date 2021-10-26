214696ab6bf49a67aa9bc59d5d0d8e4d75d68237
Charley Benedict <charleybug23@gmail.com>
2021-10-26 12:44:34 -0600

    character buttons & fixed film order

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -2,20 +2,50 @@ import { people } from '../data/people.js'
 
 const main = document.querySelector('#main')
 
+const mainHeader = document.createElement('header')
+document.body.insertBefore(mainHeader, main)
+
+
+const allButton = document.createElement('button')
+allButton.textContent = 'All Characters'
+allButton.addEventListener('click', () => populateDOM(people))
+mainHeader.appendChild(allButton)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+mainHeader.appendChild(femaleButton)
+
+const othersButton = document.createElement('button')
+othersButton.textContent = 'Other Characters'
+othersButton.addEventListener('click', () => populateDOM(otherCharacters))
+mainHeader.appendChild(othersButton)
+
 const maleCharacters = people.filter(person => person.gender === 'male')
-console.log(maleCharacters.length)
+
 const femaleCharacters = people.filter(person => person.gender === 'female')
-console.log(femaleCharacters.length)
 
 const otherCharacters = people.filter(person => {
-  if(person.gender === 'n/a' || person.gender === 'hermaphrodite') {
+  if (
+    person.gender === 'n/a' || 
+    person.gender === 'hermaphrodite' || 
+    person.gender === 'none') {
     return person
   }
 })
 
-console.log(otherCharacters.length)
+function populateDOM(characters) {
+  // remove all previous items before populating with new ones
+  while (main.firstChild) {
+    main.removeChild(main.firstChild)
+  }
 
-people.forEach((element) => {
+characters.forEach((element) => {
   const personFig = document.createElement('figure')
   const personImg = document.createElement('img')
   let charNum = getLastNumber(element.url)
@@ -27,14 +57,7 @@ people.forEach((element) => {
   personFig.appendChild(personCaption)
 
   main.appendChild(personFig)
-  
 })
-
-function getLastNumber(url) {
-  let end = url.lastIndexOf('/')
-  let start = end - 2
-  if (url.charAt(start) === '/') {
-    start++
-  }
-  return url.slice(start, end)
 }
+
+
--- films/starwars-films.js
+++ films/starwars-films.js
@@ -1,4 +1,5 @@
 import { films } from '../data/films.js'
+import { getLastNumber } from '../utils/index.js'
 
 let filmList = document.querySelector('#filmList')
 
@@ -9,7 +10,10 @@ for (let i = 0; i < films.length; i++) {
     let figImg = document.createElement('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figCaption')
-    figCaption.textContent = films[i].title
+
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+
+    figCaption.textContent = foundFilm.title
 
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
