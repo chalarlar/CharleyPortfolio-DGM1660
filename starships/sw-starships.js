3f066ce11599afa0761f8de573b0d32b728ec438
Charley Benedict <charleybug23@gmail.com>
2021-11-03 22:28:13 -0600

    linked up all SW pages to main page

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,5 +1,5 @@
-import { starships } from "../data/starships.js"
-import { getLastNumber, removeChildren } from "../utils/index.js"
+import { starships } from '../data/starships.js'
+import { getLastNumber, removeChildren } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
@@ -8,13 +8,13 @@ const shipView = document.querySelector('.shipViewer')
 const modal = document.querySelector('.modal')
 const closeButton = document.querySelector('.modal-close')
 const modalBackground = document.querySelector('.modal-background')
-const shipMessage =document.querySelector('.shipMessage')
+const shipMessage = document.querySelector('.shipMessage')
 
 closeButton.addEventListener('click', () => modal.classList.toggle('is-active'))
 modalBackground.addEventListener('click', () => modal.classList.toggle('is active'))
 
 function populateNav() {
-  starships.forEach(starship => {
+  starships.forEach((starship) => {
     let anchorWrap = document.createElement('a')
     anchorWrap.href = '#'
     let listItem = document.createElement('li')
@@ -31,7 +31,6 @@ populateNav()
 
 function populateShipView(shipData) {
     removeChildren(shipView)
-    console.log(`You clicked on ${shipData.name}`)
     let shipImage = document.createElement('img')
     let shipName = document.createElement('div')
     shipName.className = 'shipName'
